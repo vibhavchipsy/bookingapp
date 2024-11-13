3a25b72673639cbcde33048b187db74369e9d262
shiva <shiva@shiva.com>
2024-11-13 18:36:07 +0530

    booking edit form

--- src/pages/allotment/Allotment.tsx
+++ src/pages/allotment/Allotment.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { useBookings } from '../../hooks/useBookings';
 import BookingSearch from './BookingSearch';
 import BookingItem from './BookingItem';
+import BookingEditForm from './BookingEditForm';
 
 const BookingList: React.FC = () => {
 
@@ -32,22 +33,9 @@ const BookingList: React.FC = () => {
 
             {loading && <p>Loading...</p>} {/* Show loading text */}
 
-            {currentBooking &&
-                <form onSubmit={handleSubmitEdit} className='booking-form'>
-                    <input
-                        type='text'
-                        value={currentBooking.name}
-                        onChange={(e) =>
-                            setCurrentBooking({ ...currentBooking, name: e.target.value })
-                        }
-                    />
-                    <input
-                        type='date'
-                        value={currentBooking.date}
-                        onChange={(e) => setCurrentBooking({ ...currentBooking, date: e.target.value })}
-                    />
-                    <button type='submit'>Save Changes</button>
-                </form>
+            {currentBooking && (
+                <BookingEditForm currentBooking={currentBooking} handleSubmitEdit={handleSubmitEdit} setCurrentBooking={setCurrentBooking}
+                />)
             }
         </div>
     );
